5dfb9f4da6f3f3cfd405f2a7e5044d9877f013fb
S11as <50956430+S11as@users.noreply.github.com>
2020-01-26 18:51:05 +0300

    Updates

---
+++ precache-manifest.ab5045bb50de2d0ee400bf4c6350f543.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ba67948c0e5acade46b86637050c313",
+    "url": "/blind-typer/index.html"
+  },
+  {
+    "revision": "9acf085352069144ed11",
+    "url": "/blind-typer/static/css/main.0ba0e49e.chunk.css"
+  },
+  {
+    "revision": "24befa719d4bae56a06d",
+    "url": "/blind-typer/static/js/2.f67800fc.chunk.js"
+  },
+  {
+    "revision": "f73e810844fba009bd106bf78cf93a52",
+    "url": "/blind-typer/static/js/2.f67800fc.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9acf085352069144ed11",
+    "url": "/blind-typer/static/js/main.663941ec.chunk.js"
+  },
+  {
+    "revision": "dd79b8f07536b57756e9",
+    "url": "/blind-typer/static/js/runtime-main.40b79e56.js"
+  },
+  {
+    "revision": "412340788a1ccb75d76292dab587d872",
+    "url": "/blind-typer/static/media/fone.41234078.png"
+  }
+]);
